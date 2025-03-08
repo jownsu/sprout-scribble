dc42a4967535fe81cf0ba04a0794d164b6436615
Jhones <jhonesdigno666@gmail.com>
2025-03-08 22:45:38 +0800

    Setting up NeonDB with Drizzle

---
+++ server/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
+const sql = neon(process.env.POSTGRES_URL as string);
+export const db = drizzle({ client: sql });
