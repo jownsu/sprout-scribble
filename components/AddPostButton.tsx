78772c348aaf0af785403a60b72b77509a2d4818
Jhones <jhonesdigno666@gmail.com>
2025-03-09 15:38:58 +0800

    Added shadcn

--- components/AddPostButton.tsx
+++ components/AddPostButton.tsx
@@ -1,17 +1,19 @@
 "use client";
 import { useFormStatus } from "react-dom";
+import { Button } from "./ui/button";
 
 const AddPostButton = () => {
 	const { pending } = useFormStatus();
 
 	return (
-		<button 
+		<Button 
+            variant={"secondary"}
             type="submit" 
             className="bg-white text-black px-3 py-2 disabled:opacity-50"
             disabled={pending}
         >
 			Add Post
-		</button>
+		</Button>
 	);
 };
 
