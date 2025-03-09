10199d0d31ce5d591b0bfde10b63a27935abc6c1
Jhones <jhonesdigno666@gmail.com>
2025-03-09 14:59:09 +0800

    Mutating and revalidating

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import createPost from "@/server/actions/create-post";
 import getPosts from "@/server/actions/get-posts";
 import React from "react";
 
@@ -8,7 +9,18 @@ const HomePage = async () => {
 		return <div>{posts.message}</div>;
 	}
 
-	return <div>{posts.data?.map((post) => <h2 key={post.id}>{post.title}</h2>)}</div>;
+	return (
+		<div>
+			<div className="mb-4">
+				{posts.data?.map((post) => <h2 key={post.id}>{post.title}</h2>)}
+			</div>
+
+			<form action={createPost} className="flex gap-2">
+				<input type="text" name="title" className="border text-black bg-white px-2" />
+				<button type="submit" className="bg-white text-black px-3 py-2">Add Post</button>
+			</form>
+		</div>
+	);
 };
 
 export default HomePage;
