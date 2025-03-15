7fc62828b897850ae644c410709c3c17eb24b6d9
Jhones <jhonesdigno666@gmail.com>
2025-03-15 19:35:24 +0800

    Sign In Button

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,19 +1,30 @@
 import { auth } from "@/server/auth";
 import Logo from "./logo";
 import UserButton from "./user-button";
+import { Button } from "../ui/button";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
 const Nav = async () => {
 	const session = await auth();
 
 	return (
-		<header className="bg-slate-500 py-4">
+		<header className="bg-slate-500 py-8 px-3">
 			<nav>
-				<ul className="flex justify-between">
+				<ul className="flex justify-between items-center">
 					<li>
 						<Logo />
 					</li>
 					<li>
-						<UserButton user={session?.user} />
+						{session ? (
+							<UserButton user={session?.user} />
+						) : (
+							<Button asChild>
+								<Link href={"/auth/login"}>
+									<LogIn /> <span>Login</span>
+								</Link>
+							</Button>
+						)}
 					</li>
 				</ul>
 			</nav>
